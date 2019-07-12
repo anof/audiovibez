47060989fdc997fcf9eae08d9208474544f038bf
anof <anof1998@gmail.com>
2019-07-12 15:46:24 +0300

    added mic vibration support

--- app/src/main/java/com/test/audiovibez/MainActivity.java
+++ app/src/main/java/com/test/audiovibez/MainActivity.java
@@ -10,6 +10,9 @@ import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.media.effect.Effect;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -53,11 +56,12 @@ public class MainActivity extends AppCompatActivity {
     TextView chooseFile;
     TextView pauseResume;
     TextView micStartStop;
+    Vibrator vibrator;
 
 
     final int RECORDER_SAMPLE_RATE = 44100;
     final int RECORDER_CHANNELS = 1;
-    final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_8BIT;
     final int bufferSize = 2 * AudioRecord.getMinBufferSize(RECORDER_SAMPLE_RATE,
             RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING);
     final byte[] buffer = new byte[bufferSize];
@@ -79,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
             initPlayer();
             initRecorder();
             initManager();
+            initVibrator();
             configButtons();
 
             /*Player Buttons*/
@@ -104,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
                     else if(STATUS_MEDIA_PLAYER == STATE_PLAYING || STATUS_MEDIA_PLAYER == STATE_PAUSE)
                     {
                         player.stop();
+                        visualizerManager.stop();
                         startStop.setText("START");
                         pauseResume.setText("PAUSE");
                         STATUS_MEDIA_PLAYER = STATE_STOP;
@@ -126,12 +132,14 @@ public class MainActivity extends AppCompatActivity {
                     if(STATUS_MEDIA_PLAYER == STATE_PLAYING)
                     {
                         player.pause();
+                        visualizerManager.pause();
                         STATUS_MEDIA_PLAYER = STATE_PAUSE;
                         pauseResume.setText("RESUME");
                     }
                     else if(STATUS_MEDIA_PLAYER == STATE_PAUSE)
                     {
                         player.start();
+                        visualizerManager.resume();
                         STATUS_MEDIA_PLAYER = STATE_PLAYING;
                         pauseResume.setText("PAUSE");
                     }
@@ -159,6 +167,7 @@ public class MainActivity extends AppCompatActivity {
                     else if(STATUS_AUDIO_RECORD == STATE_PLAYING)
                     {
                         recorder.stop();
+                        visualizerManager.stop();
                         micStartStop.setText("START");
                         STATUS_AUDIO_RECORD = STATE_STOP;
                     }
@@ -203,6 +212,11 @@ public class MainActivity extends AppCompatActivity {
     {
         visualizerManager = new NierVisualizerManager();
         STATUS_MANAGER = STATE_NOTHING;
+    }
+
+    public void initVibrator()
+    {
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     public void updateManager(int session, int state)
@@ -271,6 +285,8 @@ public class MainActivity extends AppCompatActivity {
 
                     recorder.read(recorderByteBuffer, 0, recorderByteBuffer.length);
 
+                    vibrate(recorderByteBuffer);
+
                     int tempCounter = 0;
                     int addValue = recorderByteBuffer.length / (audioLength + mBuffer.length);
                     for (int i = 0; i <= recorderByteBuffer.length - 1 ; i += addValue) {
@@ -301,6 +317,66 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void vibrate(byte[] audioData)
+    {
+        int RMS = computeRMS(audioData);
+        int mapped = mappedValue(RMS);
+
+
+
+
+        Log.d("MyLogs", "RMS: " + mapped);
+        long[] time = new long[]{100};
+        int[] pattern = new int[]{mapped};
+        vibrator.vibrate(VibrationEffect.createWaveform(time, pattern, -1));
+
+
+    }
+
+    public int computeRMS(byte[] audioData)
+    {
+        /** Computes the RMS volume of a group of signal sizes */
+        long lSum = 0;
+        for (int i = 0; i < audioData.length; i++) {
+            lSum = lSum + audioData[i] * audioData[i];
+        }
+
+        double avg = lSum / audioData.length;
+        double rootMeanSquare = Math.sqrt(avg);
+
+        int value = computePosValueOnly(rootMeanSquare);
+
+        return value;
+    }
+
+    public int computePosValueOnly(double RMS)
+    {
+        int value;
+        int raw = (int) RMS;
+
+        value = raw - 127;
+        value = Math.abs(value);
+
+        return value;
+    }
+
+    public int mappedValue(int RMS)
+    {
+        int max = 255;
+
+        double value;
+
+        value = (RMS/30.0) * (max);
+
+        value *= 2;
+
+        value = (value > max)? max: value;
+        value = (value < 35)? 0: value;
+
+
+        return (int) value;
+    }
+
     public void configButtons()
     {
         startStop = findViewById(R.id.tv_media_player_start_or_stop);
@@ -315,12 +391,23 @@ public class MainActivity extends AppCompatActivity {
 
     public void permissions()
     {
+
+        String[] requiredPermissions = new String[]{
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.VIBRATE
+        };
+
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.RECORD_AUDIO, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            ActivityCompat.requestPermissions(this, requiredPermissions, 1);
         }
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.RECORD_AUDIO, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            ActivityCompat.requestPermissions(this, requiredPermissions, 2);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.VIBRATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, requiredPermissions, 3);
         }
     }
 }
