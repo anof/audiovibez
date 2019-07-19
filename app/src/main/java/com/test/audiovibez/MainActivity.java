7ed6d7fa32ccedc07a8fd00f4f71fe686e63ffe7
anof <anof1998@gmail.com>
2019-07-19 21:26:24 +0300

    added amplify option

--- app/src/main/java/com/test/audiovibez/MainActivity.java
+++ app/src/main/java/com/test/audiovibez/MainActivity.java
@@ -3,14 +3,11 @@ package com.test.audiovibez;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.graphics.Paint;
+import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
-import android.media.MediaPlayer;
 import android.media.MediaRecorder;
-import android.media.effect.Effect;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.support.annotation.Nullable;
@@ -24,39 +21,32 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import me.bogerchan.niervisualizer.NierVisualizerManager;
 import me.bogerchan.niervisualizer.renderer.IRenderer;
-import me.bogerchan.niervisualizer.renderer.circle.CircleBarRenderer;
-import me.bogerchan.niervisualizer.renderer.circle.CircleRenderer;
 import me.bogerchan.niervisualizer.renderer.columnar.ColumnarType1Renderer;
-import me.bogerchan.niervisualizer.renderer.columnar.ColumnarType4Renderer;
-import me.bogerchan.niervisualizer.renderer.line.LineRenderer;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
     final int STATE_PLAYING = 0;
-    final int STATE_PAUSE = 1;
     final int STATE_STOP = 2;
-    final int STATE_ERROR = 3;
-    final int STATE_PLAYER = 4;
+
     final int STATE_RECORDER = 5;
     final int STATE_NOTHING = 6;
 
 
     int STATUS_AUDIO_RECORD;
-    int STATUS_MEDIA_PLAYER;
     int STATUS_MANAGER;
 
     NierVisualizerManager visualizerManager;
-    MediaPlayer player;
     AudioRecord recorder;
     SurfaceView surfaceView;
-    TextView startStop;
-    TextView chooseFile;
-    TextView pauseResume;
     TextView micStartStop;
+    TextView ampEnableDisable;
     Vibrator vibrator;
+    boolean amplify;
 
 
     final int RECORDER_SAMPLE_RATE = 44100;
@@ -64,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
     final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_8BIT;
     final int bufferSize = 2 * AudioRecord.getMinBufferSize(RECORDER_SAMPLE_RATE,
             RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING);
-    final byte[] buffer = new byte[bufferSize];
     final int SAMPLING_RATE = 44100;
 
 
@@ -80,80 +69,13 @@ public class MainActivity extends AppCompatActivity {
         try
         {
 
-            initPlayer();
             initRecorder();
             initManager();
             initVibrator();
             configButtons();
 
-            /*Player Buttons*/
-            startStop.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
 
-                    if(STATUS_MEDIA_PLAYER == STATE_STOP)
-                    {
-                        try {
-                            player.prepare();
-                            player.start();
-                            updateManager(player.getAudioSessionId(), STATE_PLAYER);
-                            startStop.setText("STOP");
-                            STATUS_MEDIA_PLAYER = STATE_PLAYING;
-                            STATUS_MANAGER = STATE_PLAYER;
-
-                        }catch (Exception e){
-                            STATUS_MEDIA_PLAYER = STATE_ERROR;
-                            Log.d("MyLogsStartStop", e.toString());
-                        }
-                    }
-                    else if(STATUS_MEDIA_PLAYER == STATE_PLAYING || STATUS_MEDIA_PLAYER == STATE_PAUSE)
-                    {
-                        player.stop();
-                        visualizerManager.stop();
-                        startStop.setText("START");
-                        pauseResume.setText("PAUSE");
-                        STATUS_MEDIA_PLAYER = STATE_STOP;
-                        STATUS_MANAGER = STATE_NOTHING;
-                    }
-                    else if(STATUS_MEDIA_PLAYER == STATE_ERROR)
-                    {
-                        CharSequence text = "Something went wrong, please close the app and reopen it";
-                        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
-                        startStop.setText("ERROR");
-                    }
-
-                }
-            });
-
-            pauseResume.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                    if(STATUS_MEDIA_PLAYER == STATE_PLAYING)
-                    {
-                        player.pause();
-                        visualizerManager.pause();
-                        STATUS_MEDIA_PLAYER = STATE_PAUSE;
-                        pauseResume.setText("RESUME");
-                    }
-                    else if(STATUS_MEDIA_PLAYER == STATE_PAUSE)
-                    {
-                        player.start();
-                        visualizerManager.resume();
-                        STATUS_MEDIA_PLAYER = STATE_PLAYING;
-                        pauseResume.setText("PAUSE");
-                    }
-                    else if(STATUS_MEDIA_PLAYER == STATE_ERROR)
-                    {
-                        CharSequence text = "Something went wrong, please close the app and reopen it";
-                        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
-                        startStop.setText("ERROR");
-                    }
-
-                }
-            });
-
-            /*Recorder Buttons*/
+            /*Recorder Button*/
             micStartStop.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -174,6 +96,25 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
+            /*Amplify button*/
+            ampEnableDisable.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(amplify)
+                    {
+                        ampEnableDisable.setText("Amplify Disabled");
+                        ampEnableDisable.setBackgroundColor(Color.parseColor("#d9534f"));
+                        amplify = !amplify;
+                    }
+
+                    else if(!amplify)
+                    {
+                        ampEnableDisable.setText("Amplify Enabled");
+                        ampEnableDisable.setBackgroundColor(Color.parseColor("#5cb85c"));
+                        amplify = !amplify;
+                    }
+                }
+            });
 
         }
         catch (Exception e)
@@ -183,26 +124,13 @@ public class MainActivity extends AppCompatActivity {
 
     }// end main
 
-    public void initPlayer()
-    {
-
-        try {
-            player = new MediaPlayer();
-            player. setDataSource(getResources().openRawResourceFd(R.raw.demo_audio));
-//            player.setDataSource("/storage/326C-1D15/ambulance.mp3");
-            STATUS_MEDIA_PLAYER = STATE_STOP;
-
-        }catch (Exception e)
-        {
-            STATUS_MEDIA_PLAYER = STATE_ERROR;
-            Log.d("MyLogsStartStop", e.toString());
-        }
-    }
 
     public void initRecorder()
     {
         recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLING_RATE,
                 AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_8BIT, bufferSize);
+
+
 
         STATUS_AUDIO_RECORD = STATE_STOP;
 
@@ -213,6 +141,8 @@ public class MainActivity extends AppCompatActivity {
         visualizerManager = new NierVisualizerManager();
         STATUS_MANAGER = STATE_NOTHING;
     }
+
+
 
     public void initVibrator()
     {
@@ -225,20 +155,15 @@ public class MainActivity extends AppCompatActivity {
         initManager();
         int vState = -1;
 
-
-        if(state == STATE_PLAYER) {
-            vState = visualizerManager.init(session);
-        }
-        else if(state == STATE_RECORDER)
+        if(state == STATE_RECORDER)
         {
 
             vState = visualizerManager.init(new NierVisualizerManager.NVDataSource() {
 
+
                 byte[] mBuffer = new byte[512];
                 byte[] recorderByteBuffer = new byte[bufferSize/2];
                 int audioLength = (int) (recorderByteBuffer.length * 1000F / SAMPLING_RATE);
-
-
 
 
                 /**
@@ -310,7 +235,7 @@ public class MainActivity extends AppCompatActivity {
 //            visualizerManager.start(surfaceView, new IRenderer[]{new CircleBarRenderer()});
 
         } else {
-            CharSequence text = "Something went wrong with visual manager, please Stop and play audio";
+            CharSequence text = "Something went wrong with visual manager, please Stop and Start MIC";
             Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
 
         }
@@ -323,12 +248,13 @@ public class MainActivity extends AppCompatActivity {
         int mapped = mappedValue(RMS);
 
 
-
-
-        Log.d("MyLogs", "RMS: " + mapped);
-        long[] time = new long[]{100};
+        long[] time = new long[]{50};
         int[] pattern = new int[]{mapped};
-        vibrator.vibrate(VibrationEffect.createWaveform(time, pattern, -1));
+        if(mapped == 0)
+            vibrator.cancel();
+        else
+            vibrator.vibrate(VibrationEffect.createWaveform(time, pattern, -1));
+
 
 
     }
@@ -367,11 +293,13 @@ public class MainActivity extends AppCompatActivity {
         double value;
 
         value = (RMS/30.0) * (max);
+        value = (value < 20)? 0: value; // noise cancellation
 
-        value *= 2;
+        Log.d("MyLogsMapped", "RMS: " + value);
+        if(amplify)
+            value *= 1.5;
 
         value = (value > max)? max: value;
-        value = (value < 35)? 0: value;
 
 
         return (int) value;
@@ -379,14 +307,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void configButtons()
     {
-        startStop = findViewById(R.id.tv_media_player_start_or_stop);
-        chooseFile = findViewById(R.id.tv_choose_file);
-        pauseResume = findViewById(R.id.tv_media_player_pause_or_resume);
         micStartStop = findViewById(R.id.tv_audio_record_start_or_stop);
-
+        ampEnableDisable = findViewById(R.id.amplifyBtn);
         surfaceView = findViewById(R.id.sv_wave);
         surfaceView.setZOrderOnTop(true);
         surfaceView.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        amplify = false;
     }
 
     public void permissions()
@@ -394,7 +320,6 @@ public class MainActivity extends AppCompatActivity {
 
         String[] requiredPermissions = new String[]{
                 Manifest.permission.RECORD_AUDIO,
-                Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.VIBRATE
         };
 
@@ -402,9 +327,6 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, requiredPermissions, 1);
         }
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, requiredPermissions, 2);
-        }
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.VIBRATE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, requiredPermissions, 3);
